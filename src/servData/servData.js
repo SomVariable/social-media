f88c7869581fc0c92d89e8b84a12386704195cc3
SomVariable <79557545+SomVariable@users.noreply.github.com>
2021-12-21 22:03:34 +0200

    Created Users Page

---
+++ src/servData/servData.js
@@ -0,0 +1,5 @@
+let data = {
+    cityes: ["New York", "Los Angeles", "Chicago", "Miami", "Dallas", "Philadelphia", "Houston", "Atlanta", "Washington", "Boston", "Phoenix"]
+};
+
+export default data
